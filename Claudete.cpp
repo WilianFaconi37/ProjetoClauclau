427512da3922bd01907e77ce758d4f5b86d19720
unknown <000395@TERM3-15.LABFAE.local>
2014-08-28 22:25:19 -0300

    Fiz belas modificacoes

--- Claudete.cpp
+++ Claudete.cpp
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 main( ) {
-      printf("Meu nome e Claudete\n");
+      char nome[30];
+      strcpy(nome,"Claudete");
+      printf("Meu nome e %s\n", nome);
       system("PAUSE>>NULL");
       }
